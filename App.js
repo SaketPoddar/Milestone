cb72be88bb2d44c5039ef973b740e313eef44e2b
Saket Poddar <saketpoddar@Sakets-MacBook-Air.local>
2025-03-29 11:46:50 +0530

    added one comment

--- App.js
+++ App.js
@@ -1,6 +1,9 @@
 import React from "react";
 import { NavigationContainer } from "@react-navigation/native";
 import AppNavigator from "./src/navigation/AppNavigator";
+
+//nikhil check changes
+
 const App = () => {
   return (
     <NavigationContainer>
